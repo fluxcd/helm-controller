3e558b3fc4e4d2c5f7227407824fe02266330e99
Stefan Prodan <stefan.prodan@gmail.com>
2021-03-25 13:31:13 +0200

    Remove deprecated log-json flag
    Signed-off-by: Stefan Prodan <stefan.prodan@gmail.com>

--- main.go
+++ main.go
@@ -76,7 +76,6 @@ func main() {
 	flag.BoolVar(&watchAllNamespaces, "watch-all-namespaces", true,
 		"Watch for custom resources in all namespaces, if set to false it will only watch the runtime namespace.")
 	flag.IntVar(&httpRetry, "http-retry", 9, "The maximum number of retries when failing to fetch artifacts over HTTP.")
-	flag.CommandLine.MarkDeprecated("log-json", "Please use --log-encoding=json instead.")
 	clientOptions.BindFlags(flag.CommandLine)
 	logOptions.BindFlags(flag.CommandLine)
 	leaderElectionOptions.BindFlags(flag.CommandLine)
