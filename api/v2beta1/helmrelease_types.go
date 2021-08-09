89a240bf51d11c9600748af0f101aee9d65154eb
Hidde Beydals <hello@hidde.co>
2021-08-09 16:11:28 +0200

    Revert "Merge pull request #294 from makkes/default-observed-generation"
    This reverts commit 58c1ec5412977fb88a5ab8bde8a2fe9a885b83c8, reversing
    changes made to 098fa6d4d1b8b4be4f163b22de39d335d77612d0.
    
    Signed-off-by: Hidde Beydals <hello@hidde.co>

--- api/v2beta1/helmrelease_types.go
+++ api/v2beta1/helmrelease_types.go
@@ -881,8 +881,7 @@ type HelmRelease struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
 
-	Spec HelmReleaseSpec `json:"spec,omitempty"`
-	// +kubebuilder:default:={"observedGeneration":-1}
+	Spec   HelmReleaseSpec   `json:"spec,omitempty"`
 	Status HelmReleaseStatus `json:"status,omitempty"`
 }
 
