afe6f4cf2cbd9a6cf4354aca7e1127c3402ae2d8
Jiri Tyr <jiri.tyr@gmail.com>
2022-07-28 11:56:10 +0100

    Moving stuff from runner; removing changes in v2beta1
    Signed-off-by: Jiri Tyr <jiri.tyr@gmail.com>

--- internal/action/test.go
+++ internal/action/test.go
@@ -49,6 +49,20 @@ func newTest(config *helmaction.Configuration, obj *v2.HelmRelease, opts []TestO
 	test.Namespace = obj.GetReleaseNamespace()
 	test.Timeout = obj.Spec.GetTest().GetTimeout(obj.GetTimeout()).Duration
 
+	filters := make(map[string][]string)
+
+	for _, f := range obj.Spec.GetTest().GetFilters() {
+		name := "name"
+
+		if f.Exclude {
+			name = "!" + name
+		}
+
+		filters[name] = append(filters[name], f.Name)
+	}
+
+	test.Filters = filters
+
 	for _, opt := range opts {
 		opt(test)
 	}
