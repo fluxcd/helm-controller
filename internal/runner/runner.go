fluxcd/helm-controller

commit 3b25041385934e6ba7fc8e2eabf9ea17cd9d9474

runner: configure Helm action cfg log levels
This reduces the amount of log lines pushed to `debug` by configuring the kube
client and storage loggers to only log to `trace`.

In addition, the log buffer used in events will now just contain the
most relevant information about a failure as reported by the Helm action
itself, and not the in-depth information from the underlying client
and/or storage.

Signed-off-by: Hidde Beydals <hidde@hhh.computer>

internal/runner/runner.go
@@ -42,6 +42,8 @@ import (
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 
+	runtimelogger "github.com/fluxcd/pkg/runtime/logger"
+
 	v2 "github.com/fluxcd/helm-controller/api/v2beta1"
 	"github.com/fluxcd/helm-controller/internal/features"
 )
@@ -74,12 +76,15 @@ type Runner struct {
 // namespace configured to the provided values.
 func NewRunner(getter genericclioptions.RESTClientGetter, storageNamespace string, logger logr.Logger) (*Runner, error) {
 	runner := &Runner{
-		logBuffer: NewLogBuffer(NewDebugLog(logger).Log, defaultBufferSize),
+		logBuffer: NewLogBuffer(NewDebugLog(logger.V(runtimelogger.DebugLevel)).Log, defaultBufferSize),
 	}
-	runner.config = new(action.Configuration)
-	if err := runner.config.Init(getter, storageNamespace, "secret", runner.logBuffer.Log); err != nil {
+	cfg := new(action.Configuration)
+	if err := cfg.Init(getter, storageNamespace, "secret", NewDebugLog(logger.V(runtimelogger.TraceLevel)).Log); err != nil {
 		return nil, err
 	}
+	// Override the logger used by the Helm actions with the log buffer.
+	cfg.Log = runner.logBuffer.Log
+	runner.config = cfg
 	return runner, nil
 }
 

internal/runner/log_buffer.go
@@ -37,7 +37,7 @@ func NewDebugLog(log logr.Logger) *DebugLog {
 }
 
 func (l *DebugLog) Log(format string, v ...interface{}) {
-	l.log.V(1).Info(fmt.Sprintf(format, v...))
+	l.log.Info(fmt.Sprintf(format, v...))
 }
 
 type LogBuffer struct {

commit eb3a4b84cdb45042ea4f4a299f179a55f6292149

runner: address regression in captured Helm logs
This addresses a regression in the Helm log capturing introduced in
3b25041385934e6ba7fc8e2eabf9ea17cd9d9474, which prevented valuable
information from the Kube client logs (e.g. the specific reason for a
timeout) to be added to the event emitted in case of a failure.

Signed-off-by: Hidde Beydals <hidde@hhh.computer>

internal/runner/runner.go
@@ -78,13 +78,22 @@ func NewRunner(getter genericclioptions.RESTClientGetter, storageNamespace strin
 	runner := &Runner{
 		logBuffer: NewLogBuffer(NewDebugLog(logger.V(runtimelogger.DebugLevel)), defaultBufferSize),
 	}
+
+	// Default to the trace level logger for the Helm action configuration,
+	// to ensure storage logs are captured.
 	cfg := new(action.Configuration)
 	if err := cfg.Init(getter, storageNamespace, "secret", NewDebugLog(logger.V(runtimelogger.TraceLevel))); err != nil {
 		return nil, err
 	}
-	// Override the logger used by the Helm actions with the log buffer.
+
+	// Override the logger used by the Helm actions and Kube client with the log buffer,
+	// which provides useful information in the event of an error.
 	cfg.Log = runner.logBuffer.Log
+	if kc, ok := cfg.KubeClient.(*kube.Client); ok {
+		kc.Log = runner.logBuffer.Log
+	}
 	runner.config = cfg
+
 	return runner, nil
 }
 